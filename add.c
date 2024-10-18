d6ca1219d75782c0f91543933a468e81a7b14add
Ruqhiya2409 <ruqhiyataj123@gmail.com>
2024-10-18 15:03:34 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	printf("SIC class\n");
+	printf("Hello world\n");
+	printf("Welcome");
+	printf("Addition of two numbers\n")
+int a,b,sum;
+a=10;
+b=35;
+printf("a =%d ",a);
+printf("\nb =%d ",b);
+sum=a+b;
+printf("\nSum of a and b :%d ",sum);
+}
